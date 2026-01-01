16e02f9e524d9210137126e118b1b68024b609b1
Gustavo Piñones <gustipinones@192.168.1.25>
2026-01-01 19:56:24 -0300

    Desactivar Image Optimization de Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,11 @@ const nextConfig = {
     }
   },
   images: {
+    unoptimized: true, // Desactivar Image Optimization de Vercel
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: 'images.unsplash.com' }
     ],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 3600, // 1 hora de caché
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
